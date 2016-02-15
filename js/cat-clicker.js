4af28d8049048fb05235ccb4115d963c405ebb54
Marina <marinaciee@yahoo.com>
2016-02-15 18:05:36 -0500

    docs: Add description

--- js/cat-clicker.js
+++ js/cat-clicker.js
@@ -1,4 +1,4 @@
-//Each time a cat picture is clicked, the function increments the number of clicks
+//Each time the cat picture is clicked, the function increments the number of clicks displayed in countDisplay element.
 var count = 0;
 countDisplay = document.getElementById("countDisplay");
 function clicker(){
