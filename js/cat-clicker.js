9732d86c916c739ce149c22d36c67f3b6bebd300
Marina <marinaciee@yahoo.com>
2016-02-16 23:18:44 -0500

    refactor: Use EventListener function on click

--- js/cat-clicker.js
+++ js/cat-clicker.js
@@ -1,7 +1,9 @@
 //Each time the cat picture is clicked, the function increments the number of clicks displayed in countDisplay element.
 var count = 0;
 countDisplay = document.getElementById("countDisplay");
-function clicker(){
-	count++;
-	countDisplay.innerHTML = count;
-}
+var elem = document.getElementById('catimg');
+elem.addEventListener('click', function(){
+    //the element has been clicked... count the clicks
+    count++;
+    countDisplay.innerHTML = count;
+}, false);
